48b277abed8c0f20978e398768e7d0a807b8c3b2
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-02 18:06:47 +0300

    End of the Seminar

--- src/Controller/Command.java
+++ src/Controller/Command.java
@@ -1,2 +1,11 @@
-package Controller;public enum Command {
+package Controller;
+
+public enum Command { // Команды для работы со списком
+    NONE,
+    READ,
+    CREATE,
+    UPDATE,
+    LIST,
+    DELETE,
+    EXIT
 }
