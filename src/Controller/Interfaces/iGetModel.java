d855a730b2cb11d6c8a2bf7a3e8497c3a477e43e
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-03-02 18:06:58 +0300

    End of the Seminar

--- src/Controller/Interfaces/iGetModel.java
+++ src/Controller/Interfaces/iGetModel.java
@@ -1,2 +1,8 @@
-package Controller.Interfaces;public interface iGetModel {
+package Controller.Interfaces;
+
+import Model.Domain.Student;
+import java.util.List;
+
+public interface iGetModel { // Интерфейс нужен для ModelClass
+    public List<Student> getAllStudent();
 }
